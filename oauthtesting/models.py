57cb1095fd9118fe3fa027eabf1f66fc60547c17
Jar3dC0nway <hat7wd@virginia.edu>
2023-10-20 17:51:09 -0400

    Added several new models to models.py. Also added a few tests to tests.py to test some of these new models. Also also added .idea to gitignore

--- oauthtesting/models.py
+++ oauthtesting/models.py
@@ -6,24 +6,107 @@ from django.db import models
 
 USERNAME_LENGTH = 64
 BIOGRAPHY_LENGTH = 200
-IMAGE_FILE_LOCATION = 'images'
 
+TEXT_LENGTH = 200  # Max text message length
+COORDINATE_DECIMAL_PLACES = 6  # How many digits after the decimal place should longitude/latitude have?
+
+IMAGE_FILE_LOCATION = 'images'
+DRAWING_FILE_LOCATION = 'drawing'
+
+DEFAULT_POINTS = 1  # How many points should a point of interest (POI) give?
 
 # https://stackoverflow.com/questions/51570254/django-change-name-of-image-from-imagefield
 # Referenced to figure out how to access the username (line 26 and method account_url)
 
+
 def account_url(instance, image):
-  filetype = image.split('.')[1]
-  return "{}/{}.{}".format(IMAGE_FILE_LOCATION, instance.username, filetype)
+    filetype = image.split('.')[-1]
+    return "{}/{}.{}".format(IMAGE_FILE_LOCATION, instance.username, filetype)
+
 
 class Account(models.Model):
-  # Main info
-  username = models.CharField(max_length=USERNAME_LENGTH)
-  points = models.IntegerField()
+    # Main info
+    username = models.CharField(max_length=USERNAME_LENGTH, unique=True, primary_key=True)
+    points = models.IntegerField()
 
-  # Optional
-  bio = models.CharField(max_length=BIOGRAPHY_LENGTH)
-  picture = models.ImageField(upload_to=account_url)
+    # Optional
+    bio = models.CharField(max_length=BIOGRAPHY_LENGTH)
+    picture = models.ImageField(upload_to=account_url)
 
-  def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
+
+
+class Friends(models.Model):
+    user1 = models.ForeignKey(Account, related_name="user1", on_delete=models.CASCADE)
+    user2 = models.ForeignKey(Account, related_name="user2", on_delete=models.CASCADE)
+
+    # Primary Key
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['user1', 'user2'], name='friend_key')]
+
+    def __str__(self):
+        return str(self.user1) + " is friends with " + str(self.user2)
+
+
+# Point of Interest
+class POI(models.Model):
+    pid = models.IntegerField(primary_key=True)
+    time = models.DateTimeField()
+    points = models.IntegerField(default=DEFAULT_POINTS)
+    longitude = models.DecimalField(max_digits=COORDINATE_DECIMAL_PLACES + 2,
+                                    decimal_places=COORDINATE_DECIMAL_PLACES)
+    latitude = models.DecimalField(max_digits=COORDINATE_DECIMAL_PLACES + 2,
+                                   decimal_places=COORDINATE_DECIMAL_PLACES)
+
+    def __str__(self):
+        return "Point of Interest at " + str(self.longitude) + ", " + str(self.latitude) + " worth " + str(self.points)
+
+
+class Captured(models.Model):
+    pid = models.ForeignKey(POI, on_delete=models.CASCADE, primary_key=True)
+    username = models.ForeignKey(Account, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.username) + " captured " + str(self.time)
+
+
+class Message(models.Model):
+    username = models.ForeignKey(Account, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    longitude = models.DecimalField(max_digits=COORDINATE_DECIMAL_PLACES + 2,
+                                    decimal_places=COORDINATE_DECIMAL_PLACES)
+    latitude = models.DecimalField(max_digits=COORDINATE_DECIMAL_PLACES + 2,
+                                   decimal_places=COORDINATE_DECIMAL_PLACES)
+
+    # Primary Key
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['username', 'time'], name='message_id')]
+
+
+class TextMessage(Message):
+    text = models.CharField(max_length=TEXT_LENGTH)
+
+    def __str__(self):
+        return str(self.username) + ": " + str(self.text)
+
+
+class DrawingMessage(Message):
+    data = models.FileField(upload_to=DRAWING_FILE_LOCATION)
+
+    def __str__(self):
+        return str(self.username) + ": " + str(self.data)
+
+
+class Like(models.Model):
+    poster = models.ForeignKey(Message, on_delete=models.CASCADE)
+    liker = models.ForeignKey(Account, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+
+    # Primary Key
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['poster', 'liker'], name='like_id')]
+
+    def __str__(self):
+        return str(self.liker) + " liked " + str(self.poster) + "'s post from " + str(self.time)
--- oauthtesting/tests.py
+++ oauthtesting/tests.py
@@ -1,13 +1,40 @@
 from django.test import TestCase
-from oauthtesting.models import Account
+from oauthtesting.models import Account, Friends, POI, Captured, Message, TextMessage, Like
+
 
 # Create your tests here.
 
 
 class ExampleTestCase(TestCase):
-  def setUp(self):
-    Account.objects.create(username = "test", points = 0, bio = "Hi, I'm a dummy account", picture = None)
+    def setUp(self):
+        user1 = Account.objects.create(username="test", points=0, bio="Hi, I'm a dummy account", picture=None)
+        user2 = Account.objects.create(username="test2", points=0, bio="I like bread", picture=None)
+        user3 = Account.objects.create(username="test3", points=121121224534, bio="", picture=None)
+        user4 = Account.objects.create(username="test4", points=0, bio="Who needs friends?", picture=None)
 
-  def test_account(self):
-    account = Account.objects.get(username = "test")
-    self.assertEqual(account.points, 0)
+        Friends.objects.create(user1=user1, user2=user2)
+        Friends.objects.create(user1=user3, user2=user1)
+
+    def test_account(self):
+        account = Account.objects.get(username="test")
+        self.assertEqual(account.points, 0)
+
+    def test_not_an_account(self):
+        account = Account.objects.filter(username="username")
+        self.assertEqual(len(account), 0)
+
+    def test_friend(self):
+        user1 = Account.objects.get(username="test")
+        user2 = Account.objects.get(username="test2")
+        friend = Friends.objects.filter(user1=user1, user2=user2) | Friends.objects.filter(user1=user2, user2=user1)
+        self.assertEqual(len(friend), 1)
+
+    def test_multiple_friends(self):
+        user1 = Account.objects.get(username="test")
+        friend = Friends.objects.filter(user1=user1) | Friends.objects.filter(user2=user1)
+        self.assertEqual(len(friend), 2)
+
+    def test_friendless(self):
+        user4 = Account.objects.get(username="test4")
+        friend = Friends.objects.filter(user1=user4) | Friends.objects.filter(user2=user4)
+        self.assertEqual(len(friend), 0)
