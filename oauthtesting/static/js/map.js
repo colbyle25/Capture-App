f21e92c1dee9485462148bc3658bed9fb4c77e20
ethan <mud5xz@virginia.edu>
2023-10-31 20:24:20 -0400

    Added infowindows to markers allowing users to write messages and allow users to delete markers

--- oauthtesting/static/js/map.js
+++ oauthtesting/static/js/map.js
@@ -1,4 +1,8 @@
 var map;
+var markers = [];
+var uniqueID = 0;
+const MAX_MARKERS = 3;
+
 function initMap() {
 
     account_image = document.images;
@@ -101,9 +105,47 @@ function initMap() {
     });
 }
 function placeMarker(location) {
+    if (markers.length >= MAX_MARKERS){
+        alert("You can only place " + MAX_MARKERS + " markers at a time.");
+        return;
+    }
+
     var marker = new google.maps.Marker({
         position: location,
         map: map,
         draggable: true  // Allows users to drag and adjust the marker position if desired
+    })
+
+    marker.id = uniqueID;
+    marker.userMessage = "";
+    uniqueID += 1;
+
+    google.maps.event.addListener(marker, "click", function (e) {
+        var contentString = generateContentString(marker, location);
+        var infoWindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        infoWindow.open(map, marker);
     });
+
+    markers.push(marker);
+}
+function generateContentString(marker, location) {
+    var contentString = '<div class="infoWindowContent">';
+    contentString += 'Write a Message! ' + '<br>';
+    contentString += '<textarea id="userMessage_' + marker.id + '" class="userMessageTextarea">' + marker.userMessage + '</textarea><br>';
+    contentString += '<input type="button" class="saveButton" onclick="SaveMessage(' + marker.id + ');" value="Save">';
+    contentString += '<input type="button" class="deleteButton" onclick="DeleteMarker(' + marker.id + ');" value="Delete">';
+    contentString += '</div>';
+    return contentString;
+}
+
+function DeleteMarker(id) {
+    for (var i = 0; i < markers.length; i++) {
+        if (markers[i].id == id) {
+            markers[i].setMap(null);
+            markers.splice(i, 1);
+            return;
+        }
+    }
 }
